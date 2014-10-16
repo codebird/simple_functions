6372af4b12b0810c3e341758ef1ab00a7d307ed2
hmallah <hmallah@hmallah.com>
2014-10-16 15:53:04 +0300

    function to change numbers into words. Ex: 123 -> One Hunder Twenty Three.

---
+++ simple_functions/change_numbers_to_words.php
@@ -0,0 +1,50 @@
+<?php
+function conver_to_letters($number){
+	//Array to change numbers into words
+	$dictionary  = array(1=>'one', 2=>'two', 3=>'three', 4=>'four', 5=>'five', 6=>'six', 7=>'seven', 8=>'eight', 9=>'nine', 10=>'ten', 11=>'eleven', 12=>'twelve', 13=>'thirteen', 14=>'fourteen', 15=>'fifteen', 16=>'sixteen', 17=>'seventeen', 18=>'eighteen', 19=>'nineteen', 20=>'twenty', 30=>'thirty', 40=>'fourty', 50=>'fifty', 60=>'sixty', 70=>'seventy', 80=>'eighty', 90=>'ninety');
+	//Array to add after each section of the number
+	$dictionary2=array(0=>'', 1=>'thousand', 2=>'million', 3=>'billion', 4=>'trillion', 5=>'quadrillion', 6=>'quintillion');
+
+	//Converting the number to a string
+	$string_number=(string)$number;
+
+	//Calculating the number of loops needed to separate the number into chunks of 3
+	$loops=ceil(strlen($string_number)/3);
+
+	//Separating the number into chunks of 3 starting from the end.
+	for($i=0; $i<$loops; $i++){
+		$array[]=substr($string_number, -3, 3);
+		//Deleting the 3 numbers we just got from our string.
+		$string_number=substr($string_number, 0, -3);
+	}
+
+	//Looping through the array of chunks of 3 or less numbers to change each part into words.
+	for($i=0; $i<count($array); $i++){
+		$string_number=(string)$array[$i];
+		$this_string='';
+
+		//Calculating
+		if(strlen($string_number)==3){
+			$this_string=$dictionary[$string_number[0]].' hundred ';
+			$array[$i]=($array[$i]-(int)$string_number[0]*100);
+		}
+		if($array[$i]>19){
+			$tens=((int)($array[$i]/10))*10;
+			$ones=$array[$i]-$tens;
+			$this_string.=$dictionary[$tens].' '.$dictionary[$ones];
+		}
+		else {
+			$this_string.=$dictionary[$array[$i]];
+		}
+
+		//Adding the section word after our string.
+		$this_string.=' '.$dictionary2[$i];
+		if($string==''){
+			$string='and '.$this_string;
+		}
+		else {
+			$string=$this_string.', '.$string;
+		}
+	}
+	return $string;
+}
