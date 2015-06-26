32c79176b23d6515d9bf3f2ecf551d7bc618e94a
Hicham Mallah <mallah.hicham@gmail.com>
2015-06-26 19:10:32 +0300

    Update change_numbers_to_words.js
    Fixed spelling, capitalisation, and code efficiency.

--- simple_functions/change_numbers_to_words.js
+++ simple_functions/change_numbers_to_words.js
@@ -2,41 +2,36 @@ function convert_to_letters(number){
 	//Make sure number is a string.
 	number=number.toString()
 	//Array to change numbers into words
-	dictionary  = {0:'', 1:'One', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 30:'thirty', 40:'fourty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety'};
+	dictionary  = {0:'', 1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five', 6:'Six', 7:'Seven', 8:'Eight', 9:'Nine', 10:'Ten', 11:'Eleven', 12:'Twelve', 13:'Thirteen', 14:'Fourteen', 15:'Fifteen', 16:'Sixteen', 17:'Seventeen', 18:'Eighteen', 19:'Nineteen', 20:'Twenty', 30:'Thirty', 40:'Forty', 50:'Fifty', 60:'Sixty', 70:'Seventy', 80:'Eighty', 90:'Ninety'};
 	//Array to add after each section of the number
-	dictionary2= {0:'', 1:'thousand', 2:'million', 3:'billion', 4:'trillion', 5:'quadrillion', 6:'quintillion'};
+	dictionary2= {0:'', 1:'Thousand', 2:'Million', 3:'Billion', 4:'Trillion', 5:'Quadrillion', 6:'Quintillion'};
 	loops=Math.ceil(number.length/3);
 	array=new Array()
 	string=''
 	//Separating the number into chunks of 3 starting from the end.
 	for(i=0; i<loops; i++){
-		array[i]=number.substring( (number.length)-3, (number.length));
+		part=number.substring( (number.length)-3, (number.length));
 		//Deleting the 3 numbers we just got from our string.
 		number=number.substring(0, (number.length)-3);
-	}
-
-	//Looping through the array of chunks of 3 or less numbers to change each part into words.
-	for(i=0; i<array.length; i++){
-		string_number=array[i];
 		this_string='';
 		
-		if(parseInt(array[i])!=0){
+		if(parseInt(part)!=0){
 			//Calculating
-			if(string_number.length==3 && string_number[0]!=0){
-				this_string=dictionary[string_number[0]].capitalizeFirstLetter()+' Hundred ';
-				array[i]=(array[i]-parseInt(string_number[0]*100));
+			if(part.length==3 && part[0]!=0){
+				this_string=dictionary[part[0]]+' Hundred ';
+				part=(part-parseInt(part[0]*100));
 			}
-			if(array[i]>19){
-				tens=(parseInt(array[i]/10))*10;
-				ones=parseInt(array[i])-tens;
-				this_string+=dictionary[tens].capitalizeFirstLetter()+' '+dictionary[ones].capitalizeFirstLetter();
+			if(part>19){
+				tens=(parseInt(part/10))*10;
+				ones=parseInt(part)-tens;
+				this_string+=dictionary[tens]+' '+dictionary[ones];
 			}
 			else {
-				this_string+=dictionary[parseInt(array[i])].capitalizeFirstLetter();
+				this_string+=dictionary[parseInt(part)];
 			}
 	
 			//Adding the section word after our string.
-			this_string+=' '+dictionary2[i].capitalizeFirstLetter();
+			this_string+=' '+dictionary2[i];
 			if(string==''){
 				string=this_string;
 			}
